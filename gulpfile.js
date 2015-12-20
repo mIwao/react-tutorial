7283e06bac435a42ec7cb34be9168a49acef4ded
miwao <iwao.masashi@gmail.com>
2015-12-20 15:09:41 +0900

    modified gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp')
 
 gulp.task('copy-js', function() {
   gulp.src( [
-              'node_modules/react/dist/react.min.js',
-              'node_modules/react-dom/dist/react-dom.min.js',
+              'node_modules/react/dist/react.js',
+              'node_modules/react-dom/dist/react-dom.js',
               'node_modules/babel-core/lib/api/browser.js',
               'node_modules/jquery/dist/jquery.js',
               'node_modules/marked/lib/marked.js'
